b479be17e83730d1bf80b0bf0e6386696853ed4c
kaomijikun5 <118958413+kaomojikun5@users.noreply.github.com>
2024-10-17 13:47:10 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const axiosa = require("axios");
+const app =express();
+const config = require("./server=config.json");
+
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.listen(config.PORT, () => {
+    console.log('Server started on port:${config.PORT}');
+});
+app.post("/api/translate", async (req, res) => {
+    const {data} = await axiosa.post(
+        "https://api-free.deepl.com/v2/translate", 
+        {
+            text: [req.body.text],
+            target_lang: req.body.targetLang,
+        },
+        {
+            heads: {
+                Authorization: 'DeepL-Auth-Key ${config.DEEPL_AUTH_KEY}',
+            },
+        }
+    );
+    res.json({ text: data.translations[0].text})
+});
